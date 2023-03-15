c0f63953080c7bd9aab5a0ad4eb0b5d412f4e4f3
josscmorera <63267272+josscmorera@users.noreply.github.com>
2023-03-15 14:08:28 -0400

    dino

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 // Change the span with the class `mess-with-me-more` to have a font size of 40px.
-const spanMessWithMeMore = document.querySelector('.mess-with-me-more');
-spanMessWithMeMore.style.fontSize = '40px';
+let span = document.querySelector('.mess-with-me');
+span.style.fontSize = '40px';
 
 // Change the paragraph with the class `mess-with-me` to have a background color of green.
-const paragraphMessWithMe = document.querySelector('.mess-with-me');
-paragraphMessWithMe.style.backgroundColor = 'green';
+let paragraph = document.querySelector('.mess-with-me-paragraph');
+paragraph.style.backgroundColor = 'green';
 
 // Hide the second dinosaur's image so that it's no longer visible, and so that the area below it moves up to take the space.
-const hideMeArea = document.querySelector('#hide-me-area');
-hideMeArea.style.height = 0;
+let hideMe = document.querySelector('#hide-me-area');
+hideMe.style.height = 0;
 
 // Change the first dinosaur image so that the image's dimensions are 324 pixels wide.
-const triceratopsImg = document.querySelector('#triceratops');
-triceratopsImg.style.width = '324px';
+let triImg = document.querySelector('#triceratops');
+triImg.style.width = '324px';
 
 // Add an event listener to the span with the class `mess-with-me-more` so that when it is clicked, the font color changes to orange.
-spanMessWithMeMore.addEventListener('click', function() {
-  spanMessWithMeMore.style.color = 'orange';
+span.addEventListener('click', function() {
+  span.style.color = 'orange';
 });
 
 // Add an event listener to the first dinosaur so that when they are clicked, they receive a red border.
-const firstDinosaurImg = document.querySelector('#triceratops');
-firstDinosaurImg.addEventListener('click', function() {
-  firstDinosaurImg.style.border = '5px solid red';
+let firstDinoImg = document.querySelector('#triceratops');
+firstDinoImg.addEventListener('click', function() {
+  firstDinoImg.style.border = '5px solid red';
 });
 
 // Add an event listener to the feathered dinosaur, so that when they are clicked, they become 50% transparent.
-const featheredDinosaurImg = document.querySelector('#feathers');
-featheredDinosaurImg.addEventListener('click', function() {
-  featheredDinosaurImg.style.opacity = '0.5';
+let featheredImg = document.querySelector('#feathers');
+featheredImg.addEventListener('click', function() {
+  featheredImg.style.opacity = '0.5';
 });
 
 // Add an event listener to the "Switch Background Color" button so that when it is clicked, we switch the background color of the row of dinosaurs to a color of your choice.
-const toggleButton = document.querySelector('#toggle');
-const dinosaurRow = document.querySelector('#row');
+let toggleButton = document.querySelector('#toggle');
+let dinosaurRow = document.querySelector('#row');
 let toggled = false;
 
 toggleButton.addEventListener('click', function() {
@@ -47,12 +47,12 @@ toggleButton.addEventListener('click', function() {
 });
 
 // Add an event listener to the dinosaur image with id "biggify" to increase its width to 200 pixels when it is hovered over
-const biggifyDinosaurImg = document.querySelector('#biggify');
-biggifyDinosaurImg.addEventListener('mouseover', function() {
-  biggifyDinosaurImg.style.width = '200px';
+let biggifyImg = document.querySelector('#biggify');
+biggifyImg.addEventListener('mouseover', function() {
+  biggifyImg.style.width = '200px';
 });
 
-biggifyDinosaurImg.addEventListener('mouseout', function() {
-  biggifyDinosaurImg.style.width = '162px';
+biggifyImg.addEventListener('mouseout', function() {
+  biggifyImg.style.width = '162px';
 });
 
